13e8c8e0806bf44c39d400c7f36a28bd40b8ae8b
Prabin K.C. <kcprabin2063@gmail.com>
2026-02-06 07:00:39 +0545

    fir origranne 1

---
+++ prog_1.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX 100
+
+char stack[MAX];
+int top = -1;
+
+
+void push(char ch) {
+    stack[++top] = ch;
+}
+
+char pop() {
+    return stack[top--];
+}
+
+int isMatching(char open, char close) {
+    if (open == '(' && close == ')') return 1;
+    if (open == '{' && close == '}') return 1;
+    if (open == '[' && close == ']') return 1;
+    return 0;
+}
+
+int isBalanced(char exp[]) {
+    for (int i = 0; i < strlen(exp); i++) {
+        char ch = exp[i];
+        if (ch == '(' || ch == '{' || ch == '[') {
+            push(ch);
+        }
+        else if (ch == ')' || ch == '}' || ch == ']') {
+            if (top == -1) 
+                return 0;
+
+            char open = pop();
+            if (!isMatching(open, ch))
+                return 0;
+        }
+    }
+
+
+    return (top == -1);
+}
+
+int main() {
+    char exp[MAX];
+
+    printf("Enter expression: ");
+    gets(exp); 
+
+    if (isBalanced(exp))
+        printf("Expression is BALANCED\n");
+    else
+        printf("Expression is NOT BALANCED\n");
+
+    return 0;
+}
